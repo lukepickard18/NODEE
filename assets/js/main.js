c8b2d0a740d9a030f47610c46ca39c7a79b49311
Luke Pickard <pickli1@student.op.ac.nz>
2021-09-28 14:19:36 +1300

    added a procfile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,24 @@
-let txts = document.querySelectorAll("#container > div");
-txts.forEach(div => {
-    div.addEventListener("click", (event) => {      
-        let page = event.target.querySelector(".page");
+document.addEventListener("click", (event) => {
+
+    if(event.target.matches(".page"))
+    {
+        let page = event.target;
         
         if(page.innerHTML == "Browse VW Collection")
         {
             console.log("page 1");
             // put all your logic to show the right page
-                document.querySelector("#pagetwo > div").style.display = "flex";
-           
+            document.querySelector("#pagetwo > div").style.display = "grid"; 
+            
         }
         else
         {
             console.log("page another");
         }
-
+        let txts = document.querySelectorAll("#container > div");
         txts.forEach(t => {
             t.style.fontSize = "3vw";
         })
-    })
-})
+    }
+
+});
